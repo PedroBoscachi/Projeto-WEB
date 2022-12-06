0ab691ec6956623c0d63665a0b25d1faac9070bc
Lucas Lima <lucas.dbz.lima@hotmail.com>
2022-12-05 21:03:22 -0300

    fix: r4esponsive

--- Clinica/frontend/clinica/src/app/pages/signup/signup.component.ts
+++ Clinica/frontend/clinica/src/app/pages/signup/signup.component.ts
@@ -48,6 +48,10 @@ export class SignupComponent implements OnInit {
       this.signupForm.value.password as string
     );
 
+    if (!this.isValidCPF(this.signupForm.value.cpf as string)) {
+      return this.snackBar.openSnackBar('Digite um cpf válido', 'OK');
+    }
+
     if (
       this.signupForm.value.password !== this.signupForm.value.confirmPassword
     ) {
@@ -64,5 +68,40 @@ export class SignupComponent implements OnInit {
       this.snackBar.openSnackBar('Cadastrado com sucesso!', 'OK');
       this.router.navigate(['/']);
     }
+  }
+
+  isValidCPF(cpf: string) {
+    if (typeof cpf !== 'string') return false;
+    cpf = cpf.replace(/[\s.-]*/gim, '');
+    if (
+      !cpf ||
+      cpf.length != 11 ||
+      cpf == '00000000000' ||
+      cpf == '11111111111' ||
+      cpf == '22222222222' ||
+      cpf == '33333333333' ||
+      cpf == '44444444444' ||
+      cpf == '55555555555' ||
+      cpf == '66666666666' ||
+      cpf == '77777777777' ||
+      cpf == '88888888888' ||
+      cpf == '99999999999'
+    ) {
+      return false;
+    }
+    var soma = 0;
+    var resto;
+    for (var i = 1; i <= 9; i++)
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != parseInt(cpf.substring(9, 10))) return false;
+    soma = 0;
+    for (var i = 1; i <= 10; i++)
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != parseInt(cpf.substring(10, 11))) return false;
+    return true;
   }
 }
